4770ff4b541db54801ccb58b5941e82bc02bd48c
Matthew Love <matthewtglove@gmail.com>
2023-12-29 18:48:04 -0600

    Start of website

---
+++ projects/site/matthew-love-site/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="welcomeMessage">
+        <h1>Welcome to Matthew Love's website!</h1>
+        <p>It's a little empty right now. More to come soon!</p>
+      </div>
+      <div>
+        <p>This website was made with Vite + React:</p>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <div className="card">
+        <p>Here's a cool button. What happens when you press it?</p>
+        <button onClick={() => setCount(count <= 1 ? (count) => count + 1 : (count) => count * count)}>
+          count is {count}
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default App
